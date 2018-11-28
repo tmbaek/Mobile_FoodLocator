9029caa3627890148c290525b0a084280466eaca
tmbaek <tmbaek@gmail.com>
2018-11-27 22:36:14 -0500

    Initial with facebook login

---
+++ FoodLocator/obj/Debug/MonoAndroid81/android/src/md5a4734618bc4a81f5e454c130f97d4e57/MainActivity.java
@@ -0,0 +1,84 @@
+package md5a4734618bc4a81f5e454c130f97d4e57;
+
+
+public class MainActivity
+	extends android.support.v7.app.AppCompatActivity
+	implements
+		mono.android.IGCUserPeer,
+		com.facebook.FacebookCallback
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onActivityResult:(IILandroid/content/Intent;)V:GetOnActivityResult_IILandroid_content_Intent_Handler\n" +
+			"n_onCancel:()V:GetOnCancelHandler:Xamarin.Facebook.IFacebookCallbackInvoker, Xamarin.Facebook.Common.Android\n" +
+			"n_onError:(Lcom/facebook/FacebookException;)V:GetOnError_Lcom_facebook_FacebookException_Handler:Xamarin.Facebook.IFacebookCallbackInvoker, Xamarin.Facebook.Common.Android\n" +
+			"n_onSuccess:(Ljava/lang/Object;)V:GetOnSuccess_Ljava_lang_Object_Handler:Xamarin.Facebook.IFacebookCallbackInvoker, Xamarin.Facebook.Common.Android\n" +
+			"";
+		mono.android.Runtime.register ("FoodLocator.MainActivity, FoodLocator", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("FoodLocator.MainActivity, FoodLocator", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public void onActivityResult (int p0, int p1, android.content.Intent p2)
+	{
+		n_onActivityResult (p0, p1, p2);
+	}
+
+	private native void n_onActivityResult (int p0, int p1, android.content.Intent p2);
+
+
+	public void onCancel ()
+	{
+		n_onCancel ();
+	}
+
+	private native void n_onCancel ();
+
+
+	public void onError (com.facebook.FacebookException p0)
+	{
+		n_onError (p0);
+	}
+
+	private native void n_onError (com.facebook.FacebookException p0);
+
+
+	public void onSuccess (java.lang.Object p0)
+	{
+		n_onSuccess (p0);
+	}
+
+	private native void n_onSuccess (java.lang.Object p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
